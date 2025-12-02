7b5ad9ff75710487b7ba74b77c6bdc5a37cc6614
Muhammad Abdulrahman <stillkhaleefah@gmail.com>
2025-12-02 11:36:12 +0000

    Try fixing this error: `Runtime Error: TypeError: Cannot read properties

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,30 +1,13 @@
 
 import { NextResponse, type NextRequest } from "next/server";
 import { authentication } from "next-firebase-auth-edge/lib/next/middleware";
-
-const commonOptions = {
-    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
-    cookieName: 'auth-token',
-    cookieSignatureKeys: [process.env.COOKIE_SIGNATURE_KEY_A!, process.env.COOKIE_SIGNATURE_KEY_B!],
-    cookieSerializeOptions: {
-        path: "/",
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "lax" as const,
-        maxAge: 12 * 60 * 60 * 24, // 12 days
-    },
-    serviceAccount: {
-        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
-        clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-        privateKey: process.env.FIREBASE_ADMIN_PRIVATE_KEY!.replace(/\\n/g, '\n'),
-    },
-};
+import { serverConfig } from "./lib/firebase/config.edge";
 
 export async function middleware(request: NextRequest) {
     return authentication(request, {
         loginPath: "/api/login",
         logoutPath: "/api/logout",
-        ...commonOptions,
+        ...serverConfig,
         handleValidToken: async ({ token, decodedToken }) => {
             const pathname = request.nextUrl.pathname;
             const headers = new Headers(request.headers);
